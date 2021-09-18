fd79a21aab93a1b6a5ea4a682a3b2a1a439b9688
Karan Gohil <karandilipsinhgohil@gmail.com>
2021-09-18 12:55:04 -0700

    Fixed the mistake

--- division.py
+++ division.py
@@ -1,11 +1,8 @@
-import pytest  
+  import unittest
 
 def divison(first_number,second_number):
     if second_number==0:
         return -1
     else:
-        return first_number,second_number
+        return first_number/second_number
 
-def test_division():
-    assert divison(5,0) == 0
-    assert divison(5,1) == 5
