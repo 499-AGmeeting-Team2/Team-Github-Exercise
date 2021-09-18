9ffd678b3c778a34880cbd64c1c9910bceca1919
sirusw <50982809+ourinsou@users.noreply.github.com>
2021-09-18 13:36:42 -0700

    update

--- test_main.py
+++ test_main.py
@@ -1,2 +1,15 @@
-import test_main
+import unittest
+from addition import add_numbers
+import random
 
+
+class test_main(unittest.TestCase):
+
+    def test_sum(self):
+        a = random.randint(1, 100)
+        b = random.randint(1, 100)
+        return add_numbers(a, b) == a + b
+
+
+if __name__ == '__main__':
+    test_main.main()
