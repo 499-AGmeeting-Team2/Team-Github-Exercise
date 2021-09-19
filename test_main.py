6310e1d20405b6eff17acd3b5da8aa47777533e7
sirusw <50982809+sirusw@users.noreply.github.com>
2021-09-19 12:03:23 -0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -18,10 +18,10 @@ class test_main(unittest.TestCase):
 
     def test_subtraction(self):
         return subtraction(7, 3) == 4
- 
+
     def test_division(self):
-        self.assertEqual(division(10,5) == 2)
-        self.assertEqual(division(10,0) == -1)
+        self.assertEqual(division(10, 5) == 2)
+        self.assertEqual(division(10, 0) == -1)
 
 
 if __name__ == '__main__':
