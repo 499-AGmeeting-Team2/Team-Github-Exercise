ea626cdb17bf9d6ad8d07457abb6d16172a63595
Karan Gohil <karandilipsinhgohil@gmail.com>
2021-09-19 11:42:28 -0700

    added test case for division in test_main.py

--- division.py
+++ division.py
@@ -1,5 +1,5 @@
 
-def divison(first_number, second_number):
+def division(first_number, second_number):
     if second_number == 0:
         return -1
     else:
--- test_main.py
+++ test_main.py
@@ -1,16 +1,20 @@
 import unittest
 from addition import add_numbers
 from subtraction import subtraction
+from division import division
 
 
-class TestFunctions(unittest.TestCase):
+class testFunctions(unittest.TestCase):
 
     def test_sum(self):
         return add_numbers(3, 7) == 10
 
     def test_subtraction(self):
         return subtraction(7, 3) == 4
-
+    
+    def test_division(self):
+        return division(10,2) == 5
+        
 
 if __name__ == '__main__':
-    TestFunctions.main()
+    testFunctions.main()
