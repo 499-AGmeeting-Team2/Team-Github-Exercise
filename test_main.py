e8507a656abb31bef358e9bccaa7a9dc58f1c271
jayantpuri <jayantpuri16@gmail.com>
2021-09-19 16:04:10 +0530

    Added unittest fo multiply

--- test_main.py
+++ test_main.py
@@ -1,2 +1,13 @@
-import test_main
+import unittest
+from multiply import multiply
+
+
+class testMultiply(unittest.TestCase):
+    
+    def test_multiply(self):
+        self.assertEqual(multiply(10, 5), 50)
+        
+        
+if '__main__'==__name__:
+    unittest.main()
 
