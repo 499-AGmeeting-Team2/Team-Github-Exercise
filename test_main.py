a9d4b485e15f988a63278dc2ed40ab33e6801979
Karan Gohil <karandilipsinhgohil@gmail.com>
2021-09-19 11:58:42 -0700

    Fixed the test cases for division

--- test_main.py
+++ test_main.py
@@ -1,19 +1,10 @@
 import unittest
-from multiply import multiply
-
 from addition import add_numbers
 from subtraction import subtraction
 from division import division
 
 
-<<<<<<< HEAD
 class testFunctions(unittest.TestCase):
-=======
-class test_main(unittest.TestCase):
-
-    def test_multiply(self):
-        self.assertEqual(multiply(10, 5), 50)
->>>>>>> 212582635119f6512c7d672c56a75111975e344d
 
     def test_sum(self):
         return add_numbers(3, 7) == 10
@@ -22,12 +13,8 @@ class test_main(unittest.TestCase):
         return subtraction(7, 3) == 4
     
     def test_division(self):
-        return division(10,2) == 5
+        self.assertEqual(division(10,5) == 2)
+        self.assertEqual(division(10,0) == -1)
         
-
 if __name__ == '__main__':
-<<<<<<< HEAD
     testFunctions.main()
-=======
-    test_main.main()
->>>>>>> 212582635119f6512c7d672c56a75111975e344d
